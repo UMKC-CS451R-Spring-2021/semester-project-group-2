a28ab95e820bba3badf338bddc37470e0060a53a
coykwan <coykwan@gmail.com>
2021-04-19 13:47:50 -0500

    combined models into the front end, still need to add logic

--- commerce-site/banking/views.py
+++ commerce-site/banking/views.py
@@ -3,6 +3,31 @@ from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
+def login(request):
+    return render(request, "registration/login.html")
+
+def about(request):
+    return render(request, "commerce/about.html")
+
+def contact(request):
+    return render(request, "commerce/contact.html")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'commerce/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+@login_required
+def transactions(request):
+	query_results=Transactions.objects.all() # Collect all records from table 
+	return render(request,'commerce/transactions.html',{'query_results':query_results})
+
+########
 
 def login(request):
     return render(request, 'homepage.html')
