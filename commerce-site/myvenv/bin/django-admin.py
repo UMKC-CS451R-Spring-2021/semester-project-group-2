235b2430a6f21ff3eb4fefb160098b35d525d414
Daylan Quinn <daylanq10@gmail.com>
2021-05-07 20:45:11 -0500

    Final Upload
    A (mostly) completed project

--- commerce-site/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/coy/Development/semester-project-group-2/commerce-site/myvenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
